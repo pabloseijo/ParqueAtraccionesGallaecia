6018607ea0673b522bbd93c0923da94c97777f51
Pablo Seijo <105173542+Vehkzyr@users.noreply.github.com>
2023-04-14 11:37:38 +0200

    Add files via upload

---
+++ src/GUI/Login/LoginGUI.java
@@ -0,0 +1,25 @@
+package Login;
+
+import javax.swing.*;
+
+public class LoginGUI extends JFrame{
+    private JTextField textField1;
+    private JTextField textField2;
+    private JButton entrarButton;
+    private JLabel Usuario;
+    private JLabel Password;
+    private JPanel Panel1;
+
+    public LoginGUI(){
+        setContentPane(Panel1);
+        setTitle("Introduzca Credenciales");
+        setSize(450,300);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+
+    public static void main(String[] args){
+        LoginGUI myLogin = new LoginGUI();
+    }
+
+}
