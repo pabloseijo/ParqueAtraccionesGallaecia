679948b094f8ba91ee6ed1f9fe8510b23b30b188
Miguel Leal Fernández <130046052+MiguiLeal8@users.noreply.github.com>
2023-04-20 13:41:17 +0200

    Add files via upload

--- src/GUI/UserPage.java
+++ src/GUI/UserPage.java
@@ -1,4 +1,55 @@
 package GUI;
 
-public class UserPage {
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class UserPage extends JFrame{
+
+    private JButton SALIRButton;
+    private JLabel IconoUsuario;
+    private JLabel Pulpo;
+    private JLabel NombreUsuario;
+    private JLabel IconoAtracciones;
+    private JButton ATRACCIONESButton;
+    private JLabel IconoEspectaculos;
+    private JLabel IconoHosteleria;
+    private JLabel IconoEntradas;
+    private JButton ESPECTÁCULOSButton;
+    private JButton HOSTELERÍAButton;
+    private JButton ENTRADASButton;
+
+    public UserPage() {
+
+        ATRACCIONESButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        ESPECTÁCULOSButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        HOSTELERÍAButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        ENTRADASButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        SALIRButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dispose();
+            }
+        });
+    }
 }
