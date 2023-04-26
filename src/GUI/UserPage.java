7100d12006f32c05df25900f1752398861c6d102
Miguel Leal Fernández <130046052+MiguiLeal8@users.noreply.github.com>
2023-04-26 19:55:37 +0200

    FIX BUGS

--- src/GUI/UserPage.java
+++ src/GUI/UserPage.java
@@ -1,15 +1,11 @@
 package GUI;
 
-import baseDatos.FachadaBaseDatos;
-
 import javax.swing.*;
-import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class UserPage extends JFrame{
 
-    private FachadaBaseDatos fachadaBaseDatos;
     private JButton SALIRButton;
     private JLabel IconoUsuario;
     private JLabel Pulpo;
@@ -18,26 +14,11 @@ public class UserPage extends JFrame{
     private JButton ESPECTACULOSButton;
     private JButton HOSTELERIAButton;
     private JButton ENTRADASButton;
-
-    private JPanel Jpanel1;
-    private JPanel MainPanel;
     private JButton COMPRARENTRADASButton;
     private JButton VOLVERButton;
+    private JPanel MainPanel;
 
-    public UserPage(JFrame parent, FachadaBaseDatos fachadaBaseDatos) {
-        ////////////////////////////////////////////////
-        // ARREGLAR NON FUNCIONA
-        ////////////////////////////////////////////
-        //ponemos el titulo de la pestaña
-        setTitle("Usuario");
-        //Mostramos el panel del .form
-        setContentPane(MainPanel);
-        //Ponemos el tamaño de la ventana
-        setMinimumSize(new Dimension(700,500));
-        //Centramos la pestaña
-        setLocationRelativeTo(parent);
-        //Esto hace que se cierre al darle a la X
-        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+    public UserPage() {
 
         ATRACCIONESButton.addActionListener(new ActionListener() {
             @Override
